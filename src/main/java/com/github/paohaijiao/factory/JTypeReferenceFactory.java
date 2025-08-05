8195a7447f7d66d60ab24672d4b8ac7d8444174c
martin <goudingcheng@gmail.com>
2025-08-05 19:19:52 +0800

    submit the colloction type to the jquick lang

---
+++ src/main/java/com/github/paohaijiao/factory/JTypeReferenceFactory.java
@@ -0,0 +1,4 @@
+package com.github.paohaijiao.factory;
+
+public class JTypeReferenceFactory {
+}
---
+++ src/main/java/com/github/paohaijiao/model/JTypeReferenceAndValueModel.java
@@ -0,0 +1,7 @@
+package com.github.paohaijiao.model;
+
+import lombok.Data;
+
+@Data
+public class JParamModel {
+}
